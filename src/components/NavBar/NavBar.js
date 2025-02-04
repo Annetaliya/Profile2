c2a4210cfb807984797532fff6ec9eed8d8dd9b6
Annette <annetaliya@gmail.com>
2025-02-04 14:07:02 -0800

    add nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './navbar.css'
+
+const navItems = [
+    {item: 'Home', to: '#home'},
+    {item: 'About', to: '#about'},
+    {item: 'Portfolio', to: '#portfolio'},
+    {item: 'Contact', to: '#contact'},
+]
+
+const NavBar = () => {
+  return (
+    <nav className='navBar'>
+        <div>AA</div>
+        <ul className='list-container'>
+            {navItems.length !== 0 ? 
+            navItems.map((item, index) => (
+                <li key={index}>
+                    {item.item}
+                </li>
+            ))
+            :<p>No items found</p> }
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
