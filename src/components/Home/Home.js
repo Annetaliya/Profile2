da6e89cdb8736a74d92b62112300355696bd6f53
Annette <annetaliya@gmail.com>
2025-02-06 13:03:02 -0800

    add skills details

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './home.css';
+import { FaFigma } from "react-icons/fa";
+import { RiComputerLine } from "react-icons/ri";
 
 const data = {name: 'Annette Adhiambo', 
               profession: 'Front-end Engineer',
@@ -12,13 +14,26 @@ const data = {name: 'Annette Adhiambo',
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <div>
-        <p><span className='greetings'>Hello</span> My name is {data.name}</p>
-        <p>{data.profession}</p>
+    <div className='parent-home-container'>
+      <div className='home-container'>
+        <div>
+          <p><span className='greetings'>Hello</span> My name is {data.name}</p>
+          <p>{data.profession}</p>
+          <p className='intro-text'>I'm a skilled frontend website developer with<br></br>
+          experience in creating modern and responsive websites</p>
+        </div>
+        <div>
+          <img src={data.photo} alt='profile' loading='lazy'/>
+        </div>
       </div>
-      <div>
-        <img src={data.photo} alt='profile' loading='lazy'/>
+      <h1 className='heading-intro'>What I do</h1>
+      <div className='skills'>
+        <FaFigma size={30} />
+        <p>{data.skills.skill1}</p>
+      </div>
+      <div className='skills'>
+        <RiComputerLine size={30}/>
+        <p>{data.skills.skill2}</p>
       </div>
 
     </div>
