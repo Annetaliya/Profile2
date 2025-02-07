748f1038026a7170a6d7bc9cce775b407c59b5c6
Annette <annetaliya@gmail.com>
2025-02-07 12:30:18 -0800

    add about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar/NavBar"
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
     
     </div>
   );
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import './about.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='about-intro'>About Me</h1>
+      <div className='about-container'>
+        <div className='socials'>
+          <FaLinkedin size={30}/>
+          <FaGithub size={30}/>
+        </div>
+        
+        <div>Welcome to my portfolio website</div>
+        <div className='skills-intro'>I specialize in front-end engineering with proficiency in JavaScript,
+            HTML and CSS. I am highly skilled in working with React, including state
+            management and have experience working with bootstrap to create
+            responsive and visually appealing websites.
+        </div>
+      
+      </div>
+    </div>
   )
 }
 
