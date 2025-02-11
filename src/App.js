d50d95e654f06489a58eed16f1dc3f375f89b276
Annette <annetaliya@gmail.com>
2025-02-10 21:00:50 -0800

    add portfolio component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar/NavBar"
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Portfolio />
     
     </div>
   );
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
 
+const PortfolioItems = [
+  {
+    name: 'E-Commerce Website',
+    github: 'https://github.com/Annetaliya/React-ecommerce',
+    website: 'https://659968189ed589e081611c57--jocular-valkyrie-82c7de.netlify.app/',
+    photo:  "https://lh3.googleusercontent.com/d/1FPxAUkC4HDTHW4dvyoAiBuO-Up1HDsAj=s220?authuser=0",
+  },
+  {
+    name: 'Landing Page',
+    github: 'https://github.com/Annetaliya/Landing-Page',
+    website: 'https://landing-page-i6zw.vercel.app/',
+    photo: 'https://lh3.googleusercontent.com/d/1m-clqocWaseCcEDDoKXn4Q9-jYL1K45K=s220?authuser=0',
+  },
+  {
+    name: 'E-Commerce Website',
+    github: '#home',
+    website: '#home',
+    photo: 'https://lh3.googleusercontent.com/d/1fh4JQGs0CjtHkG4d7QmXxlbAK_7WRQkz=s220?authuser=0',
+  },
+]
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div>
+      <h1>My Portfolio</h1>
+      <p> I take pride in paying attention to the smallest details and making sure
+        my work is pixel perfect. I am excited to bring my skills and experience
+        to help businesses achieve their goals</p>
+        <div>
+          {PortfolioItems.length !==0 ? 
+          PortfolioItems.map((elements, index) => (
+            <div key={index}>
+              <img src={elements.photo} alt='website' loading='lazy'/>
+              <p>{elements.name}</p>
+              <a href={elements.website} target='_blank' rel="noreferrer">
+                <button>Website</button>
+              </a>
+              <a href={elements.github} target='_blank' rel="noreferrer">
+                <button>Github</button>
+              </a>
+            </div>
+          ))
+          
+          : <p>No items to display</p>}
+        </div>
+
+    </div>
   )
 }
 
